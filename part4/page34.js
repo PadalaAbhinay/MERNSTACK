2d695d26f2f251280ff508ecac6ce20b6fbf950a
PadalaAbhinay <padalaabhinay5@gmail.com>
2026-03-06 12:00:16 +0530

    updated

--- part4/page34.js
+++ part4/page34.js
@@ -2,7 +2,42 @@ import express from "express"
 const app = express()
 app.set("view engine","ejs")
 app.set("views","./views")
-app.listen(8080,()=> console.log("server is started "))
-app.get("/",(req,res)=>{
+app.use(express.urlencoded({extended:true}));
+
+const users = [
+    { name:"Abhinay", email:"abhinay@gmail.com", password:"1234" },
+    { name:"Abhishek", email:"abhishek@gmail.com", password:"1234" },
+    { name:"Abhijeet", email:"abhi@gmail.com", password:"1234" },
+
+
+]
+// app.get("/",(req,res)=>{
+//     res.render("login");
+// });
+app.get("/register",(req,res)=>{
+    res.render("register");
+});
+app.get("/login",(req,res)=>{
     res.render("login");
 });
+
+app.post("/login",(req,res)=>{
+    const {email,password} = req.body;
+    const user = users.find((user) => user.email === email);
+    if(user) {
+        if(user.password === password) {
+            res.redirect("/");
+        } else {
+            res.redirect("/login");
+        }
+    }   else {
+        res.redirect("/login");
+    }
+    res.redirect("/");
+});
+
+app.get("/",(req,res)=>{
+    res.render("dashboard",{users});
+});
+
+app.listen(2000,()=> console.log("server is started "));
