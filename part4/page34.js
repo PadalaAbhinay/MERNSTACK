5ff9777f21b3e3fd6d19a7258244ff8f9efb174b
PadalaAbhinay <padalaabhinay5@gmail.com>
2026-03-06 13:33:33 +0530

    updated

--- part4/page34.js
+++ part4/page34.js
@@ -1,43 +1,45 @@
-import express from "express"
-const app = express()
-app.set("view engine","ejs")
-app.set("views","./views")
-app.use(express.urlencoded({extended:true}));
+import express from "express";
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.listen(2000, () => console.log("Server Started"));
 
-const users = [
-    { name:"Abhinay", email:"abhinay@gmail.com", password:"1234" },
-    { name:"Abhishek", email:"abhishek@gmail.com", password:"1234" },
-    { name:"Abhijeet", email:"abhi@gmail.com", password:"1234" },
+app.use(express.urlencoded({ extended: true }));
 
+let users = [
+  { name: "Poojitha", email: "poojitha@gmail.com", password: "1234" },
+  { name: "Ajay", email: "ajay@gmail.com", password: "1234" },
+  { name: "Komal", email: "komal@gmail.com", password: "1234" },
+];
 
-]
-// app.get("/",(req,res)=>{
-//     res.render("login");
-// });
-app.get("/register",(req,res)=>{
-    res.render("register");
-});
-app.get("/login",(req,res)=>{
-    res.render("login");
+app.get("/login", (req, res) => {
+  res.render("login", { error: null });
 });
 
-app.post("/login",(req,res)=>{
-    const {email,password} = req.body;
-    const user = users.find((user) => user.email === email);
-    if(user) {
-        if(user.password === password) {
-            res.redirect("/");
-        } else {
-            res.redirect("/login");
-        }
-    }   else {
-        res.redirect("/login");
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find((user) => user.email === email);
+  if (user) {
+    if (user.password === password) {
+      res.redirect("/");
+    } else {
+      res.render("login", { error: "Invalid Password" });
     }
-    res.redirect("/");
+  } else {
+    res.render("login", { error: "User not found" });
+  }
+  // res.redirect("/");
 });
 
-app.get("/",(req,res)=>{
-    res.render("dashboard",{users});
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
-app.listen(2000,()=> console.log("server is started "));
+app.post("/register", (req, res) => {
+  users = [...users, req.body];
+  res.redirect("/");
+});
+
+app.get("/", (req, res) => {
+  res.render("dashboard", { users });
+});
